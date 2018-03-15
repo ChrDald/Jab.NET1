7d7a05bdef8ab73894f62e77d4542dfc3d5870a2
CD <CD@CDD.CO>
2018-03-15 17:23:13 -0400

    * Quiz revision forgot to push

---
+++ Quiz 1 Revision/Quiz1Revision/GettersSetters/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GettersSetters
+{
+    class Person
+    {
+        private String _name;   // these variables are used to get/set data from
+        private int _age;
+
+        public Person(String name, int age)
+        {
+            _name = name;    // Name (capital N) calls the getters/setters for the Name string below, and name (lowercase) refers to the value passed to the constructor
+            _age = age;
+        }
+
+        public String Name  // c# style getters and setters
+                            // youre basically created a method that can be accesed like a variable (called Name)
+                            // when you get the value of it, it calls the getter (that returns _name)
+                            // when you set a value to it, it calls the setter
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                // you can do whatever you want when getting and setting
+                var regex = new Regex(@"(\D|\s){1,20}");    // supposed to be any character except digits, 1 to 20 characters long
+                // verify the name has the right format
+                if (!regex.IsMatch(value))
+                {
+                    throw new InvalidDataException("Invalid Name value...");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("CD", 12);
+            String GetName = p.Name;    // p.Name calls the getter that fetches the _name data
+            Console.WriteLine(GetName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Quiz 1 Revision/Quiz1Revision/ReadWriteFile/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteFile
+{
+    class Program
+    {
+        public static List<String> AppendingLines = new List<string>();
+
+        static void Main(string[] args)
+        {
+            // Reading from file
+            // first surround with try/catch
+            try
+            {
+                // save the lines (for example) in a String[] array
+                // (import System.IO to use File)
+                String[] fileLines = File.ReadAllLines(@"people.txt");
+                // the @ in the parameter means you wont need escape characters for the link
+
+                foreach (String lines in fileLines)
+                {
+                    // Do what you need to do with each individual line
+                    Console.WriteLine("Reading file...");
+
+                    // if you need to split the lines, use this for example
+                    // String[] SplitLines = lines.Split(':'); // has to be SINGLE quotes here (google why)
+
+                    AppendingLines.Add(lines);
+                    
+                }
+                // Write to the file using streamwriter
+                
+                foreach (String lines in AppendingLines)
+                {
+                    // if you use WriteAllText instead of Append, everytime write is called it deletes previous content
+                    File.AppendAllText(@"people.txt", "\n" + lines + " -> line added");
+                    Console.WriteLine("Line Added...");
+                }
+                
+
+            } catch (IOException) // not sure if this is ok or if youd want a specific exception, readAllLines can throw
+            {
+                Console.WriteLine("IO Exception, please try again...");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
