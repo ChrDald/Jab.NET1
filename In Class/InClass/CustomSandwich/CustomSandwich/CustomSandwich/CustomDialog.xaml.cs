1cf24af76448df2fc0ec0394745844c06878cc65
CD <CD@CDD.CO>
2018-03-21 15:29:25 -0400

    * Mostly done with the custom sandwich, need to read up more on custom dialogs (the returning part)

---
+++ In Class/InClass/CustomSandwich/CustomSandwich/CustomSandwich/CustomDialog.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomSandwich
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class CustomDialog : Window
+    {
+        private string _breadType;
+        public string BreadType { get => _breadType; set => _breadType = value; }
+
+        private string[] _veggies;
+        public string[] Veggies { get => _veggies; set => _veggies = value; }
+
+        
+        public CustomDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Custom_BtSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            Sandwich sandwich = new Sandwich();
+
+            sandwich.Bread = CmbBread.Text;
+            string[] SelectedVeggies = new string[3];
+
+            // Veggies
+            if (CbCucumbers.IsChecked == true)
+            {
+                SelectedVeggies[0] = (string)CbCucumbers.Content;
+            }
+            if (CbLettuce.IsChecked == true)
+            {
+                SelectedVeggies[1] = (string)CbLettuce.Content;
+            }
+            if (CbTomatoes.IsChecked == true)
+            {
+                SelectedVeggies[2] = (string)CbTomatoes.Content;
+            }
+
+            Veggies = SelectedVeggies;     // NOT SUUURE this works...
+
+            BreadType = sandwich.Bread;
+
+            DialogResult = true;
+        }
+
+        private void Custom_BtCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    
+    public class Sandwich
+    {
+        string _bread;
+        public string[] Veggies { get; set; }
+        public string[] Meat { get; set; }
+
+        public string Bread { get => _bread; set => _bread = value; }
+    }
+
+}
---
+++ In Class/InClass/CustomSandwich/CustomSandwich/CustomSandwich/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomSandwich
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtMake_Click(object sender, RoutedEventArgs e)
+        {
+            CustomDialog dialog = new CustomDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                if (dialog.DialogResult == true)
+                {
+                    Sandwich test = new Sandwich();
+                    test.Bread = dialog.BreadType;
+                    test.Veggies = dialog.Veggies;
+
+                    TblBread.Text = test.Bread;
+
+                    string SelectedVeggies = null;
+
+      
+                    foreach (string veggies in test.Veggies)
+                    {
+                        if (veggies == "")
+                        {
+                            continue;
+                        }
+                        SelectedVeggies += veggies + ", ";
+                    }
+                    MessageBox.Show(SelectedVeggies);
+
+                    TblVeggies.Text = SelectedVeggies;
+                }
+                
+            }
+
+            
+
+        }
+    }
+}
